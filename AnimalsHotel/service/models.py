acb66f70db61389e7ec12f054e310a2a021a34ea
monika <monikamarianowakowska@gmail.com>
2021-10-27 21:10:45 +0200

    add reservation-room one-to-many relation

--- AnimalsHotel/service/models.py
+++ AnimalsHotel/service/models.py
@@ -1,9 +1,24 @@
 from django.db import models
-import datetime
 
+
+
+class Reservation(models.Model):
+    pass
 
 class Room(models.Model):
+    standard_list = (('podstawowy','podstawowy'),
+                      ('sredni', 'sredni')
+                      ('wyzszy','wyzszy'),
+                     )
     price = models.DecimalField(max_digits=10, decimal_places=2)
     room_name = models.CharField(max_length=20)
     description = models.CharField(max_length=255, blank=True)
-    standard = models.CharField(max_length=20)
+    standard = models.CharField(max_length=20,choices=standard_list,default='podstawowy')
+    reservation=models.ForeignKey(Reservation, on_delete=models.CASCADE())
+
+    def __str__(self):
+        return self.room_name
+
+    class Meta:
+        verbose_name = "Pokoj"
+        verbose_name_plural = "Pokoje"
