24cda921eed2ff54484ba20f2a1f5954aa60f54c
monika <monikamarianowakowska@gmail.com>
2021-10-27 20:56:44 +0200

    add first model pokoj

--- AnimalsHotel/service/models.py
+++ AnimalsHotel/service/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+import datetime
 
-# Create your models here.
+
+class Room(models.Model):
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    room_name = models.CharField(max_length=20)
+    description = models.CharField(max_length=255, blank=True)
+    standard = models.CharField(max_length=20)
