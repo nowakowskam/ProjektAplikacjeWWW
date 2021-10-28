492e56fe21ffd6629e44443fceffd5674858edc4
monika <monikamarianowakowska@gmail.com>
2021-10-28 09:33:33 +0200

    models except user

--- AnimalsHotel/service/models.py
+++ AnimalsHotel/service/models.py
@@ -1,9 +1,17 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 
 
 class Reservation(models.Model):
-    pass
+    client= models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    date_from=models.DateField()
+    date_to=models.DateField()
+    price=models.DecimalField(max_digits=10, decimal_places=2)
+
+    class Meta:
+        verbose_name = "Rezerwacja"
+        verbose_name_plural = "Rezerwacje"
+
 
 class Room(models.Model):
     standard_list = (('podstawowy','podstawowy'),
@@ -22,3 +30,25 @@ class Room(models.Model):
     class Meta:
         verbose_name = "Pokoj"
         verbose_name_plural = "Pokoje"
+
+class AdditionalService:
+    description = models.CharField(max_length=255, blank=True)
+    service_name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.room_name
+
+    class Meta:
+        verbose_name = "Dodatkowa usluga"
+        verbose_name_plural = "Dodatkowe uslugi"
+
+
+class Order:
+    add_service=models.ForeignKey(AdditionalService, on_delete=models.CASCADE())
+    amount=models.IntegerField(max_length=20, null=True)
+    client = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    total_price=models.DecimalField(max_digits=10, decimal_places=2)
+
+    class Meta:
+        verbose_name = "Zamowieni"
+        verbose_name_plural = "Zamowienia"
