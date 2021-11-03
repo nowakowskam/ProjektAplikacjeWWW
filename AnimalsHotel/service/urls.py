85a7255841a0d400edbeb262f71d792f3bfa0f95
monika <monikamarianowakowska@gmail.com>
2021-11-03 21:15:47 +0100

    serializer room all method

--- AnimalsHotel/service/urls.py
+++ AnimalsHotel/service/urls.py
@@ -1,10 +1,14 @@
-from django.conf.urls import url
-from django.contrib import admin
 from django.urls import path
 from django.contrib.auth.decorators import login_required
 
 from . import views
 
-urlpatterns=[
-             path('room-list',views.room_list, name='room-list'),
-             ]
+urlpatterns = [
+	path('', views.api_overview, name="api-overview"),
+	path('room-list/', views.room_list, name="room-list"),
+	path('room-detail/<str:pk>/', views.room_detail, name="room-detail"),
+	path('room-create/', views.room_create, name="room-create"),
+
+	path('room-update/<str:pk>/', views.room_update, name="room-update"),
+	path('room-delete/<str:pk>/', views.room_delete, name="room-delete"),
+]
--- AnimalsHotel/service/views.py
+++ AnimalsHotel/service/views.py
@@ -4,37 +4,55 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .serializer import RoomSerializer
 from .models import Room
+
 @api_view(['GET'])
-def api_overwiew(request):
-    api_urls = {
-        'List':'/room/list/',
-        'Detail View': '/room-details/<str:pk>/',
-        'Create': '/room-create/',
-        'Update': '/room-update/<str:pk>/',
-        'Delete': '/room-delete/<str:pk>/',
-    }
-    return Response(api_urls)
+def api_overview(request):
+	api_urls = {
+		'List':'/room-list/',
+		'Detail View':'/room-detail/<str:pk>/',
+		'Create':'/room-create/',
+		'Update':'/room-update/<str:pk>/',
+		'Delete':'/room-delete/<str:pk>/',
+		}
+
+	return Response(api_urls)
 
 @api_view(['GET'])
 def room_list(request):
-    rooms = Room.object.all()
-    serializer =RoomSerializer(rooms, many=True)
-    return Response(serializer.data)
+	rooms = Room.objects.all().order_by('-id')
+	serializer = RoomSerializer(rooms, many=True)
+	return Response(serializer.data)
 
 @api_view(['GET'])
-def room_list(request, pk):
-    rooms = Room.object.all()
-    serializer =RoomSerializer(rooms, many=True)
-    return Response(serializer.data)
+def room_detail(request, pk):
+	rooms = Room.objects.get(id=pk)
+	serializer = RoomSerializer(rooms, many=False)
+	return Response(serializer.data)
 
 
+@api_view(['POST'])
+def room_create(request):
+	serializer = RoomSerializer(data=request.data)
+
+	if serializer.is_valid():
+		serializer.save()
+
+	return Response(serializer.data)
+
+@api_view(['POST'])
+def room_update(request, pk):
+	room = Room.objects.get(id=pk)
+	serializer = RoomSerializer(instance=room, data=request.data)
+
+	if serializer.is_valid():
+		serializer.save()
+
+	return Response(serializer.data)
 
 
+@api_view(['DELETE'])
+def room_delete(request, pk):
+	room = Room.objects.get(id=pk)
+	room.delete()
 
-
-
-# def about(request):
-#     return HttpResponse('about')
-#
-# def home(request):
-#     return  HttpResponse('home')
+	return Response('Item succsesfully delete!')
