2415e771ee34cc641b531c070d8ac3f6645920e0
monika <monikamarianowakowska@gmail.com>
2021-11-03 15:03:28 +0100

    basic urls, isort config

--- AnimalsHotel/service/views.py
+++ AnimalsHotel/service/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def about(request):
+    return HttpResponse('about')
+
+def home(request):
+    return  HttpResponse('home')
