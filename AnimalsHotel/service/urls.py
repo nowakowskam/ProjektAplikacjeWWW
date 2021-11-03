a9041bf125814f105011ddfdadb2d35567246ba0
monika <monikamarianowakowska@gmail.com>
2021-11-03 20:59:56 +0100

    serializer room

--- AnimalsHotel/service/urls.py
+++ AnimalsHotel/service/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url
 from django.contrib import admin
+from django.urls import path
+from django.contrib.auth.decorators import login_required
+
 from . import views
 
-urlpatterns=[url(r'^admin/', admin.site.urls),
-             url(r'^about/$',views.about),
-             url(r'^$',views.home)
+urlpatterns=[
+             path('room-list',views.room_list, name='room-list'),
              ]
--- AnimalsHotel/service/views.py
+++ AnimalsHotel/service/views.py
@@ -1,8 +1,40 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .serializer import RoomSerializer
+from .models import Room
+@api_view(['GET'])
+def api_overwiew(request):
+    api_urls = {
+        'List':'/room/list/',
+        'Detail View': '/room-details/<str:pk>/',
+        'Create': '/room-create/',
+        'Update': '/room-update/<str:pk>/',
+        'Delete': '/room-delete/<str:pk>/',
+    }
+    return Response(api_urls)
 
-def about(request):
-    return HttpResponse('about')
+@api_view(['GET'])
+def room_list(request):
+    rooms = Room.object.all()
+    serializer =RoomSerializer(rooms, many=True)
+    return Response(serializer.data)
 
-def home(request):
-    return  HttpResponse('home')
+@api_view(['GET'])
+def room_list(request, pk):
+    rooms = Room.object.all()
+    serializer =RoomSerializer(rooms, many=True)
+    return Response(serializer.data)
+
+
+
+
+
+
+
+# def about(request):
+#     return HttpResponse('about')
+#
+# def home(request):
+#     return  HttpResponse('home')
