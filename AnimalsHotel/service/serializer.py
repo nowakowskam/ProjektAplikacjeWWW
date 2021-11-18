a1ba5ab506fb42a7efec6b072368913459461e91
monika <monikamarianowakowska@gmail.com>
2021-11-18 10:52:24 +0100

    fix user serializer

--- AnimalsHotel/service/serializer.py
+++ AnimalsHotel/service/serializer.py
@@ -18,38 +18,32 @@ class AdditionalServiceSerializer(serializers.ModelSerializer):
 
 
 class OrderSerializer(serializers.ModelSerializer):
-    add_service=serializers.PrimaryKeyRelatedField(queryset=AdditionalService.objects.all())
-    client= serializers.SerializerMethodField('_user')
-
-    def _user(self, obj):
-        request = self.context.get('request', None)
-        if request:
-            return request.user
 
     class Meta:
         model = Order
         fields=['url', 'add_service', 'client', 'total_price']
 
 class ReservationSerializer(serializers.ModelSerializer):
-    owner = serializers.ReadOnlyField(source='owner.username')
-    room=serializers.PrimaryKeyRelatedField(queryset=Room.objects.all())
-    user = serializers.PrimaryKeyRelatedField(many=True, queryset=User.get_email_field_name())
+
+    # room=serializers.PrimaryKeyRelatedField(queryset=Room.objects.all())
+    # user = serializers.PrimaryKeyRelatedField(many=True, queryset=User.get_email_field_name())
 
     class Meta:
         model=Reservation
-        fields=['owner', 'date_from', 'date_to', 'price','room', 'user', 'url']
+        fields=['client', 'date_from', 'date_to', 'price','room', 'url']
+        read_only_fields=("client",)
 
-    def validate_date(self):
-        if self.date_from > self.date_to:
-            raise serializers.ValidationError("End date must be after start date.")
-        if self.date_from >date.today():
-            raise  serializers.ValidationError("The first date you can take is for today")
-
+    # def validate_date(self):
+    #     if self.date_from > self.date_to:
+    #         raise serializers.ValidationError("End date must be after start date.")
+    #     if self.date_from >date.today():
+    #         raise  serializers.ValidationError("The first date you can take is for today")
+ #TODO correct validation
 
 
 class UserSerializer(serializers.ModelSerializer):
-    # order = serializers.PrimaryKeyRelatedField(many=True, queryset=Order.objects.all())
-    # reservation=serializers.PrimaryKeyRelatedField(many=True, queryset=Reservation.objects.all())
     class Meta:
         model = User
         fields = ['id', 'username', 'url']
+
+
