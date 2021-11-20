84172a7850741425e94b55a83bd64d862fc26032
monika <monikamarianowakowska@gmail.com>
2021-11-20 12:34:52 +0100

    set Permission class IsAuthenticated or read only

--- AnimalsHotel/AnimalsHotel/settings.py
+++ AnimalsHotel/AnimalsHotel/settings.py
@@ -118,6 +118,11 @@ STATIC_URL = "/static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
+    ]
+}
 
 try:
     # Local
