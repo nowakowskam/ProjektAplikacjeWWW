64fe2024d9e95c1b7bb8a1fdc3e944eefc09ea98
Łukasz Kostek <lkostek@users.noreply.github.com>
2021-11-23 18:47:28 +0100

    price validators and date;

--- AnimalsHotel/service/models.py
+++ AnimalsHotel/service/models.py
@@ -24,7 +24,7 @@ class Room(models.Model):
         verbose_name_plural = "Pokoje"
 
     def __str__(self):
-        return self.room_name
+        return f'{self.room_name}, {self.price}'
 
 
 class Reservation(models.Model):
@@ -50,11 +50,12 @@ class AdditionalService(models.Model):
     description = models.CharField(max_length=255, blank=True)
     service_name = models.CharField(max_length=20)
 
-
-
     class Meta:
         verbose_name = "Dodatkowa usluga"
         verbose_name_plural = "Dodatkowe uslugi"
+
+    def __str__(self):
+        return self.service_name
 
 
 class Order(models.Model):
@@ -63,11 +64,7 @@ class Order(models.Model):
     add_service = models.ForeignKey(AdditionalService, on_delete=models.CASCADE)
     amount = models.DecimalField(max_digits=10, decimal_places=2, null=True)
     client = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
-    total_price = models.DecimalField(max_digits=10, decimal_places=2)
 
     class Meta:
         verbose_name = "Zamowienie"
         verbose_name_plural = "Zamowienia"
-
-
-
--- AnimalsHotel/service/serializer.py
+++ AnimalsHotel/service/serializer.py
@@ -3,40 +3,47 @@ from .models import AdditionalService,Reservation, Room, Order
 from django.contrib.auth.models import User
 from django.db import models
 from datetime import date
+import time
 
-
+def timeConverter(date):
+    """requires %Y-%m-%d"""
+    return time.strptime(date, "%Y-%m-%d")
 
 class RoomSerializer(serializers.ModelSerializer):
     class Meta:
         model = Room
         fields = ['url','standard', 'price', 'room_name', 'description']
 
+    def validate(self, data):
+        if float(data['price']) <= 50:
+            raise serializers.ValidationError("Room never will be cheaper than 50!")
+        return data
+
 class AdditionalServiceSerializer(serializers.ModelSerializer):
     class Meta:
         model=AdditionalService
         fields=['url', 'service_name', 'description']
 
-
 class OrderSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Order
-        fields=['url', 'add_service', 'client', 'total_price']
+        fields=['url', 'add_service', 'client']
 
 class ReservationSerializer(serializers.ModelSerializer):
 
     class Meta:
         model=Reservation
-        fields=['client', 'date_from', 'date_to', 'price','room', 'url', 'total_price']
+        fields=['client', 'date_from', 'date_to', 'room', 'url']
         read_only_fields=("client",)
 
-
     def validate(self, data):
-        if data['date_from'] > data['date_to']:
+        if timeConverter(str(data['date_from'])) > timeConverter(str(data['date_to'])):
             raise serializers.ValidationError("Finish must occur after start")
-        return data
- #TODO correct validation
 
+        if timeConverter(str(data['date_to'])) <= timeConverter(str(date.today())):
+            raise serializers.ValidationError("The earliest reservation starts today")
+        return data
 
 class UserSerializer(serializers.ModelSerializer):
 
